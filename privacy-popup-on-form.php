7a489cf588197376f3addf00211b7418fbe3a34a
vianey <vianeylinares@gmail.com>
2023-05-11 10:52:41 -0700

    Added plugin basic description in php initialization file.

---
+++ privacy-popup-on-form.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Plugin Name: Privacy form on popup
+* Plugin URI: 
+* Description: Privacy form on popup description
+* Version: 1.0
+* Requires at least: 5.6
+* Author: Vianey Linares
+* Author URL: 
+* Licence: GPL v2 or later
+* Licence URL: https://www.gnu.org/licences/gp;-2.0.html
+* Text Domain: privacy-popup-on-form
+* Domain Path: /languages
+*/
+
+ /*
+Privacy form on popup is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+Privacy form on popup is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with Privacy form on popup. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
