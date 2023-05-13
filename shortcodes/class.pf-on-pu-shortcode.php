47a5800cf32a5ba5801420161e81bfed0aae3461
vianey <vianeylinares@gmail.com>
2023-05-13 11:24:47 -0700

    Added custom styles display at frontend.

--- shortcodes/class.pf-on-pu-shortcode.php
+++ shortcodes/class.pf-on-pu-shortcode.php
@@ -20,6 +20,15 @@ if( ! class_exists( 'PF_on_PU_Shortcode' ) ){
             wp_enqueue_script( 'pf-on-pu-frontend-js' );
             wp_enqueue_style( 'magnific-popup-css' );
             wp_enqueue_style( 'pf-on-pu-frontend-css' );
+            add_action( 'wp_head', function(){
+                $custom_styles = ( isset( PF_on_PU_Settings::$options['pf_on_pu_custom_styles'] ) ) ? PF_on_PU_Settings::$options['pf_on_pu_custom_styles'] : '' ;
+
+                if( $custom_styles != '' ){
+                    echo "<style>";
+                    echo $custom_styles;
+                    echo "</style>";
+                }
+            }, 999 );
 
             return ob_get_clean();
 
